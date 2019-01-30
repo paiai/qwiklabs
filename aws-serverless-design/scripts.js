8af82b1c5f9285624b12da734bbab4eee50518ca
paiai <parakissai@gmail.com>
2019-01-30 13:30:19 +0900

    modify

--- aws-serverless-design/scripts.js
+++ aws-serverless-design/scripts.js
@@ -1,63 +1,71 @@
 var API_ENDPOINT = "https://3bfsoj5hbd.execute-api.ap-northeast-2.amazonaws.com/QwikDev"
 
-document.getElementById("sayButton").onclick = function(){
+window.onload = function () {
+	document.getElementById("searchButton").click();
+}
+
+document.getElementById("sayButton").onclick = function () {
 
 	var inputData = {
 		"voice": $('#voiceSelected option:selected').val(),
-		"text" : $('#postText').val()
+		"text": $('#postText').val()
 	};
 
 	$.ajax({
-	      url: API_ENDPOINT,
-	      type: 'POST',
-	      data:  JSON.stringify(inputData)  ,
-	      contentType: 'application/json; charset=utf-8',
-	      success: function (response) {
-					document.getElementById("postIDreturned").textContent="Post ID: " + response;
-	      },
-	      error: function () {
-	          alert("error");
-	      }
-	  });
+		url: API_ENDPOINT,
+		type: 'POST',
+		data: JSON.stringify(inputData),
+		contentType: 'application/json; charset=utf-8',
+		success: function (response) {
+			document.getElementById("postIDreturned").textContent = "Post ID: " + response;
+			setTimeout(function(){ document.getElementById("searchButton").click(); }, 3000);
+		},
+		error: function () {
+			alert("error");
+		}
+	});
 }
 
-
-document.getElementById("searchButton").onclick = function(){
+document.getElementById("searchButton").onclick = function () {
 
 	var postId = $('#postId').val();
 
+	if ( postId === "" ) {
+		postId = "*";
+	}
+	console.log("postId :: ", postId)
 
 	$.ajax({
-				url: API_ENDPOINT + '?postId='+postId,
-				type: 'GET',
-				success: function (response) {
+		url: API_ENDPOINT + '?postId=' + postId,
+		type: 'GET',
+		success: function (response) {
+			console.log("url :: ", API_ENDPOINT + '?postId=' + postId);
+			$('#posts tr').slice(1).remove();
 
-					$('#posts tr').slice(1).remove();
+			jQuery.each(response, function (i, data) {
 
-	        jQuery.each(response, function(i,data) {
+				var player = "<audio controls><source src='" + data['url'] + "' type='audio/mpeg'></audio>"
 
-						var player = "<audio controls><source src='" + data['url'] + "' type='audio/mpeg'></audio>"
+				if (typeof data['url'] === "undefined") {
+					var player = ""
+				}
 
-						if (typeof data['url'] === "undefined") {
-	    				var player = ""
-						}
-
-						$("#posts").append("<tr> \
+				$("#posts").append("<tr> \
 								<td>" + data['id'] + "</td> \
 								<td>" + data['voice'] + "</td> \
 								<td>" + data['text'] + "</td> \
 								<td>" + data['status'] + "</td> \
 								<td>" + player + "</td> \
 								</tr>");
-	        });
-				},
-				error: function () {
-						alert("error");
-				}
-		});
+			});
+		},
+		error: function () {
+			alert("error");
+		}
+	});
 }
 
-document.getElementById("postText").onkeyup = function(){
+document.getElementById("postText").onkeyup = function () {
 	var length = $(postText).val().length;
-	document.getElementById("charCounter").textContent="Characters: " + length;
+	document.getElementById("charCounter").textContent = "Characters: " + length;
 }
